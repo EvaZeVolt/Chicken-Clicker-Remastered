7ae24cd40eb481c3b5e34785bf8f393c00898f57
EvaZeVolt <71860413+EvaZeVolt@users.noreply.github.com>
2023-04-04 16:00:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import './style.css';
 import 'animate.css';
 import { AddShop } from './ui.js';
-
+import { loop } from './game.js'
 export default function App() {
   return (
     <div>
       <script src="ui.js" />
       <div id="border" class="action ">
-        <h1 class="eggsText"> 5351 eggs </h1>
+        <h1 class="eggsText"> 1 eggs </h1>
         <br></br>
         <br></br>
         <br></br>
         <a onClick="">
-          <img src="/chicken.png" />
+          <img
+            src="https://raw.githubusercontent.com/EvaZeVolt/Chicken-Clicker-Remastered/main/chicken.png"
+            width="350"
+            height="450"
+          />
         </a>
       </div>
       <div id="border" class="center updates">
@@ -21,6 +25,9 @@ export default function App() {
       </div>
       <div id="border" class="right shop">
         <div id="contents">
+          <AddShop Name="Smasher" Price="90" />
+          <AddShop Name="Smasher" Price="90" />
+          <AddShop Name="Smasher" Price="90" />
           <AddShop Name="Smasher" Price="90" />
           <AddShop Name="Smasher" Price="90" />
           <AddShop Name="Smasher" Price="90" />
